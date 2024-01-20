0752097fb231418328737d4a22ba3cf0b2cdccb9
LukeDCGitHub <luke.mcmann@dcmail.ca>
2024-01-20 17:06:43 -0500

    Change product link to projects
    Change product link to projects in app.js and add id's to <a> that is altered.

--- lab1/scripts/app.js
+++ lab1/scripts/app.js
@@ -3,3 +3,7 @@
   Student ID: 100893644, 100883548
   Date finished:
 */
+
+let projects = document.getElementById("projects");
+projects.setAttribute("href", "./projects.html");
+projects.innerHTML = "Projects";
