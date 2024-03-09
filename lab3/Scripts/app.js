03d6183a435b8972335cf4390b8ac8e926c07464
LukeDCGitHub <luke.mcmann@dcmail.ca>
2024-03-09 17:12:59 -0500

    Add/fix site content
    Add content to the main pages. Fix header and footer to display correct info.

--- lab3/Scripts/app.js
+++ lab3/Scripts/app.js
@@ -81,22 +81,45 @@
 
     function displayHome()
     {
-        
+      // Add content to index.html
+      document.getElementById("indexHeader").textContent = "Home Page";
+      document.getElementById("indexWelcome").textContent = "Welcome to Fake Company's Website!";
     }
 
     function displayAbout()
     {
-
+        // Add content to about.html
+        document.getElementById("aboutHeader").textContent = "About";
+        document.getElementById("lukeHeader").textContent = "Luke";
+        document.getElementById("lukeAbout").innerHTML = "Luke is on our web development, windows app and database design teams. He is proficient uisng C# and .NET, SQL, <br/>HTML, CSS, PHP, Javascript, Java and Python. He is always learning and broading his skillset. In his personal life he likes <br/>hanging out with his friends and family. Visit his <a href='https://www.linkedin.com/in/luke-mcmann-8254ab295/'>LinkedIn</a> to learn more about him.";
+        document.getElementById("blakeHeader").textContent = "Blake";
+        document.getElementById("blakeAbout").innerHTML = "Blake is our project manager. He's good at making our jobs difficult. He knows very little about programming so<br/> we're not sure why he works for us. Infact he doesn't know much of anything really. He does bring us tootsie rolls on tuesdays,<br/> so I guess he's useful for something. Here's his <a href='https://www.linkedin.com/in/blakemunro/'>LinkedIn</a>. It might show something he's good at.";
     }
 
     function displayProjects()
     {
+      // Add content to projects.html
+      document.getElementById("projectHeader").textContent = "Favorite Projects";
+      document.getElementById("projectOneHeader").textContent = "Project #1";
+      document.getElementById("projectOne").innerHTML = "This was one of the first project's that we created as a company. This project was to create a cypher program that encypted<br/>entered text into the Al Bhed language featured in Final Fantasy X. With the development team being huge fans of FFX<br/>and the project being an intresting and fun concept caused this project to be very enjoyable for us. ";
+      document.getElementById("projectTwoHeader"). textContent = "Project #2";
+      document.getElementById("projectTwo").innerHTML = "This project was the last project we did using C# and .NET. For this project we had to create a windows form that would be<br/>used to admit students into Canadian Colleges. The program stores admitted students in a .JSON file that can be used<br/>to see admitted students information.";
+      document.getElementById("projectThreeHeader").textContent = "Project #3";
+      document.getElementById("projectThree").innerHTML = "This was one of the more recent projects that our company produced. This project is a Tic-Tac-Toe board, built with C#.<br/> This ground breaking game allows players to customize their names and symbols like nothing you've seen before.<br/>This game also keeps a record of the scores for each game to determine the overall winner.";
 
     }
 
     function displayServices()
     {
-
+      // Add content to services.html
+      document.getElementById("servicesHeader").textContent = "Services";
+      document.getElementById("serviceOneHeader").innerHTML = "<br/>Website Development";
+      document.getElementById("serviceOne").innerHTML = "Here at Fake company we create custom website for our customers. These websites are built using HTML, CSS and PHP or Javascript.<br/>We also provide service to existing websites written in PHP and or Javascript. Our web development team has lots of experience<br/>creating amazing websites that serve our customers business needs. Visit our contact us page to request our services.";
+      document.getElementById("serviceHeaderTwo").innerHTML = "<br/>Windows Apps";
+      document.getElementById("serviceTwo").innerHTML = "Here at Fake company we offer windows form app creation. Using C# and .NET we build our customers dream applications.<br/>Our windows app development team as lots of experience building impressive and beautiful apps that meet our customers needs. <br/>Visit our contact us page to request our services.";
+      document.getElementById("serviceThreeHeader").innerHTML = "<br/>Database Services";
+      document.getElementById("serviceThree").innerHTML = "Here at Fake company we provide database services. Specializing in SQL we can create you a database, design you a database or modify <br/>and improve your existing database. Our team has plenty of experience desgining and implementing databases. Please visit our contact<br/>us page to request our services. ";
+      
     }
 
     function testFullName()
